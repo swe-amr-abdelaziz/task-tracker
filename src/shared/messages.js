918ce0170ea8945d86cc9b85557d0d83337b1cca
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-08-15 22:55:05 +0300

    feat: add messages file

---
+++ src/shared/messages.js
@@ -0,0 +1,9 @@
+export const messages = {
+  error: {
+    REQUIRED_TASK_DESCRIPTION: 'Task description is required.',
+    INVALID_TASK_STATUS: 'Task status is invalid.',
+    INVALID_CREATED_AT: 'CreatedAt is invalid.',
+    INVALID_UPDATED_AT: 'UpdatedAt is invalid.',
+    BASE_ENTITY_OBJECT_CREATION: 'Cannot instantiate abstract class BaseEntity directly.',
+  },
+};
