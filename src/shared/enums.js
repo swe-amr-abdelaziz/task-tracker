6fed62d28e7b6017b63edc2953c516d034f75842
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-08-17 08:36:03 +0300

    fix(ci): add TaskCommand enums

--- src/shared/enums.js
+++ src/shared/enums.js
@@ -1,3 +1,13 @@
 export const DB_FILENAME = 'tasks.json';
 
 export const DB_FILE_ENCODING = 'utf-8';
+
+export const TaskCommand = {
+  ADD: 'add',
+  UPDATE: 'update',
+  DELETE: 'delete',
+  MARK_IN_PROGRESS: 'mark-in-progress',
+  MARK_DONE: 'mark-done',
+  LIST: 'list',
+  HELP: 'help',
+};
