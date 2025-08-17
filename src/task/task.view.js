b37d9ac21c8ce9e9842ef6881b100e1993d5daf2
Amr Abdelaziz <swe.amr.abdelaziz@gmail.com>
2025-08-17 08:33:08 +0300

    fix(ci): add task view class

---
+++ src/task/task.view.js
@@ -0,0 +1,15 @@
+export class TaskView {
+  static getTasksList(status) {}
+
+  static addTask(description) {}
+
+  static updateTask(id, description) {}
+
+  static markTaskAsInProgress(id) {}
+
+  static markTaskAsDone(id) {}
+
+  static deleteTask(id) {}
+
+  static help(command) {}
+}
